7549749a076a4c836bb72e553a48250a1ff2f8d2
unknown <codeartisanlab2607@gmail.com>
2020-12-09 22:41:46 +0530

    part-3-create-database-model

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Patient
 # Register your models here.
+class PatientAdmin(admin.ModelAdmin):
+    list_display=('full_name','mobile_no','detail','precout','visit_date','next_visit_date')
+    search_fields=('full_name','mobile_no')
+admin.site.register(Patient,PatientAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Patient(models.Model):
+    full_name=models.CharField(max_length=100)
+    email=models.CharField(max_length=50)
+    mobile_no=models.CharField(max_length=50)
+    address=models.TextField()
+    detail=models.TextField()
+    precout=models.TextField()
+    visit_date=models.DateTimeField(auto_now_add=True)
+    next_visit_date=models.DateTimeField()
+
+    def __str__(self):
+        return self.full_name
