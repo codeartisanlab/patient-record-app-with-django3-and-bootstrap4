14acb15c53ea727fe34e76ec42c5defa2d9560c2
unknown <codeartisanlab2607@gmail.com>
2020-12-10 23:24:24 +0530

    part-4-create-template-and-show-model-data

--- main/views.py
+++ main/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Patient
+def home(request):
+    data=Patient.objects.all()
+    return render(request,'home.html',{'data':data})
